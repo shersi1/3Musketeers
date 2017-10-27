10c9ee77c2ef14fe84626afe4913764dfbe76cb6
shersi1 <32780412+shersi1@users.noreply.github.com>
2017-10-27 13:37:29 -0400

    Add files via upload
    This isn't the best version of what I've been working on, I'm currently trying to restore the memory on my laptop so this is what I worked on from scratch on the desktop, the other files are from when I emailed Wiley

---
+++ Brainstorming.java
@@ -0,0 +1,11 @@
+
+public class Brainstorming {
+	public static void main (String args[]){
+		Reservation First = null;
+		First.FirstName = null;
+		First.LastName = null;
+		First.setName();
+		First.getName();
+		
+	}
+}
