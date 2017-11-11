4a8dc76608404629516f6236c85d201dc6fc596b
shersi1 <32780412+shersi1@users.noreply.github.com>
2017-11-11 16:05:01 -0500

    Add files via upload

--- Inventory.java
+++ Inventory.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+//ERROR CHECKS FOR STRINGS AND INTEGERS NEEDED, ADD IGNORE CASE FOR ALL CHARACTER VALUES IN A STRING(INT INCLUDED)
+//ADD ERROR EXCEPTIONS
 public class Inventory {
 	static ArrayList<Ingredient> inStock = new ArrayList<Ingredient>();
 	static int whileCount = 0;
@@ -20,7 +22,7 @@ public class Inventory {
 	public static String ToString(int index ,ArrayList<Ingredient> inStock){
 		return inStock.get(index).name;
 	}
-	public static void AIP(Inventory stock){//searchIngredientProgram
+	public static void AIP(Inventory stock){//Keeps adding to to the inventory before it 
 		while(whileCount == 0){
 			addIngredient(stock);
 			Scanner input = new Scanner(System.in);
@@ -33,11 +35,25 @@ public class Inventory {
 			}
 		}
 	}
+	/*public static void SIP(Inventory stock){//Keeps adding to to the inventory before it 
+		while(whileCount == 0){
+			searchIngredient(stock);
+			Scanner input = new Scanner(System.in);
+			System.out.println("Would you like to keep searching ingredients? (Y/N):");
+			String ans = input.nextLine();
+			if(ans.equals("Y") || ans.equals("y")){
+				whileCount = 0;
+			}else if(ans.equals("N") || ans.equals("n")){
+				whileCount = 1;
+			}
+		}
+	}*/
 	public static void searchIngredient(Inventory stock){
 		boolean isTrue = false;
 		Scanner search = new Scanner(System.in);
 		System.out.print("What is the ingredient you want to search for?(Case Sensitive): ");
 		String s = search.nextLine();
+
 		int size = stock.inStock.size();
 		for (int i = 0; i < size; i++){
 			if(s.equals(inStock.get(i).name)){
@@ -47,26 +63,69 @@ public class Inventory {
 			}
 		}
 		if(isTrue == false){
-				Scanner input = new Scanner(System.in);
-				System.out.println("Ingredient is NOT on inventory. Do you want to add to inventory? (Y/N)");
-				String ans = input.nextLine();
-				if(ans.equals("Y") || ans.equals("y")){
-					//SIP(stock);
-					addIngredient(s,stock);
-				}else if(ans.equals("N") || ans.equals("n")){
-				}else{
-					System.out.println("You entered the wrong command, try again!");
-					searchIngredient(stock);
-				}
+			Scanner input = new Scanner(System.in);
+			System.out.println("Ingredient is NOT on inventory. Do you want to add to inventory? (Y/N)");
+			String ans = input.nextLine();
+			if(ans.equals("Y") || ans.equals("y")){
+				//SIP(stock);
+				addIngredient(s,stock);
+			}
+			else if(ans.equals("N") || ans.equals("n")){
+			}else{
+				System.out.println("You entered the wrong command, try again!");
+				searchIngredient(stock);
 			}
 		}
+	}
+	public static void searchIngredient(int I, Inventory stock){
+		boolean isTrue = false;
+		Scanner search = new Scanner(System.in);
+		System.out.print("What is the ingredient you want to search for?(Case Sensitive): ");
+		String s = search.nextLine();
+
+		int size = stock.inStock.size();
+		for (int i = 0; i < size; i++){
+			if(s.equals(inStock.get(i).name)){
+				System.out.println("Ingredient is in inventory");
+				isTrue = true;
+			}else{
+			}
+		}
+		if(isTrue == true){
+			Scanner input = new Scanner(System.in);
+			System.out.print("Do You Want To Keep Searching? (Y/N)");
+			String reply = input.nextLine();
+			if(input.equals("Y") || input.equals("y")){
+				//SIP(stock);
+				searchIngredient(stock);
+			}
+			else if(input.equals("N") || input.equals("n")){
+			}else{
+				System.out.println("You entered the wrong command, try again!");
+				searchIngredient(stock);
+			}
+		}else if(isTrue == false){
+			Scanner input = new Scanner(System.in);
+			System.out.println("Ingredient is NOT on inventory. Do you want to add to inventory? (Y/N)");
+			String ans = input.nextLine();
+			if(ans.equals("Y") || ans.equals("y")){
+				//SIP(stock);
+				addIngredient(s,stock);
+			}
+			else if(ans.equals("N") || ans.equals("n")){
+			}else{
+				System.out.println("You entered the wrong command, try again!");
+				searchIngredient(stock);
+			}
+		}
+	}
 	public static void main (String args[]){
 		Inventory Stock = new Inventory();
 		for(int i = 1; i < 3; i++){
 			addIngredient(Stock);
 		}
 		System.out.println("# of Ingredient Types: " + Stock.inStock.size());
-		//searchIngredient(Stock);
+		searchIngredient(Stock);
 		AIP(Stock);
 	}
 }
